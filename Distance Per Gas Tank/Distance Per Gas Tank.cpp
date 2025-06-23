662f62714c7ab7983022388bf4f6d116349e1de5
KietLoc <ktl20634@email.vccs.edu>
2025-06-23 15:03:21 -0400

    Add project files.

---
+++ Distance Per Gas Tank/Distance Per Gas Tank.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+double calcDistance(int gasTank, double R) //Distance = gallons × average miles per gallon
+{
+    return gasTank * R;
+}
+
+int main() {
+    const int gasTank = 20; //Tank capacity in gallons.
+    const double tMpg = 23.5; //Town Average Rate.
+    const double hwMpg = 28.9; //Highway Average Rate.
+
+    double tDistance = calcDistance(gasTank, tMpg);
+    double hwDistance = calcDistance(gasTank, hwMpg);
+
+    cout << "With a " << gasTank << "-gallon tank:" << endl;
+    cout << "In Town driving " << tMpg << " mpg would give a Distance of " << tDistance << "miles" << endl;
+    cout << "On Highway driving " << hwMpg << " mpg would give a Distance of " << hwDistance << "miles" << endl;
+
+    return 0;
+}
