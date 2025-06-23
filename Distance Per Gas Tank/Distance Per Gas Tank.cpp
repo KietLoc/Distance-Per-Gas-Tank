dc3491361387565c9b843d377179b88f0ea125de
KietLoc <ktl20634@email.vccs.edu>
2025-06-23 15:10:37 -0400

    quick comment fix

--- Distance Per Gas Tank/Distance Per Gas Tank.cpp
+++ Distance Per Gas Tank/Distance Per Gas Tank.cpp
@@ -8,9 +8,9 @@ double calcDistance(int gasTank, double R) //Distance = gallons × average miles 
 }
 
 int main() {
-    const int gasTank = 20; //Tank capacity in gallons.
-    const double tMpg = 23.5; //Town Average Rate.
-    const double hwMpg = 28.9; //Highway Average Rate.
+    const int gasTank = 20; //Tank capacity in gallons
+    const double tMpg = 23.5; //Town Average Rate
+    const double hwMpg = 28.9; //Highway Average Rate
 
     //Calculate distance possible
     double tDistance = calcDistance(gasTank, tMpg); 
